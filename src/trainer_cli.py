fe02ec0ac5996497cfe0049ab488bf58925179db
Chris Harris <Chris.Harris010@gmail.com>
2022-12-31 21:00:56 -0600

    Formatting

--- src/trainer_cli.py
+++ src/trainer_cli.py
@@ -5,12 +5,15 @@ from train import train_model
 
 logger = logging.getLogger("aicrowd.training_cli")
 
+
 def main(parsed_args: argparse.Namespace):
     train_model(**vars(parsed_args))
 
+
 def parse_args(args: Optional[List[str]] = None):
     parser = argparse.ArgumentParser(argument_default=argparse.SUPPRESS)
-    parser.add_argument("--train_data_path", type=str, help="Path to training csv file containing queries and products.")
+    parser.add_argument("--train_data_path", type=str,
+                        help="Path to training csv file containing queries and products.")
     parser.add_argument("--product_data_path", type=str, help="Path to product catalogue csv file.")
     parser.add_argument("--model_output_path", type=str, help="Path to saved model.")
     parser.add_argument("--epochs", type=int, default=15, help="Number of epochs used for training. ")
@@ -18,6 +21,7 @@ def parse_args(args: Optional[List[str]] = None):
     parser.add_argument("--embedding_dim", type=int, default=64, help="Dimension of product and query embeddings.")
     args = parser.parse_args()
     return args
+
 
 if __name__ == "__main__":
     main(parse_args())
